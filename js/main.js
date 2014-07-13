742a020bed17491f3481897ba7db370f2a328817
Will Sabido <will@dev.webisabi.co.uk>
2014-07-13 15:56:24 +0100

    Initial Commit
    Copied files from sandbox project to new repo - initial j3 template
    setup (css, fonts, images, js, folders etc., template details & images,
    index)

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// sort jquery conflict which prevented carousel rotation
+// var $ = jQuery.noConflict(); $(document).ready(function() 
+//  { $('#myCarousel').carousel({ interval: 8000, cycle: true }); }); 
